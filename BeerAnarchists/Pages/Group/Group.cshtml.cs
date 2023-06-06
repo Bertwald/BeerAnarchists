71ce21bc093b7d188f0043af8852f388ebcb9737
Bertwald <thomas_ace8@hotmail.com>
2023-06-06 16:35:55 +0200

    small aesthetic fix

--- BeerAnarchists/Pages/Group/Group.cshtml.cs
+++ BeerAnarchists/Pages/Group/Group.cshtml.cs
@@ -1,9 +1,21 @@
+using Forum.Data.Interfaces;
+using Forum.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BeerAnarchists.Pages.Group;
-
+[Authorize]
 public class GroupModel : PageModel
 {
+    private readonly IUser _userservice;
+    private readonly UserManager<ForumUser> _userManager;
+
+    public GroupModel(IUser userservice, UserManager<ForumUser> userManager) {
+        _userservice = userservice;
+        _userManager = userManager;
+    }
+
     public void OnGet()
     {
     }
